5b4c5425548ca07063d9553733d36727953c3cb2
hyosikchoi <huchoi24@naver.com>
2023-02-22 00:28:12 +0900

    Add - Usage lazyColumn and add messageList

--- app/src/main/java/com/hyosik/android/memojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/memojetpackcompose/MainActivity.kt
@@ -3,13 +3,24 @@ package com.hyosik.android.memojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hyosik.android.memojetpackcompose.ui.theme.MemoJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +28,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MemoJetpackComposeTheme {
+
+                /** 메세지 아이디 카운트 상태 */
+                val clickCount : MutableState<Int> = remember {
+                    mutableStateOf(0)
+                }
+
+                /** 전체 메세지 리스트 상태 */
+                val messageList : SnapshotStateList<Message> = remember { mutableStateListOf() }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                   Column() {
+
+                       /** 1-1 onClicked 이벤트 발생  */
+                       AddMessage(onClicked = {
+                           clickCount.value += 1
+                           val newMsg = Message(id = clickCount.value, content = "메세지 입니다 ${clickCount.value}")
+                           /** 1-2 messageList 상태 변경 발생 */
+                           messageList.add(newMsg)
+                       })
+
+                       /** 1-3 messageList 상태를 내려줍니다. */
+                       /** 2-1 onDeleteClicked 이벤트 발생 */
+                       /** 2-3 messageList 상태를 내려줍니다. */
+                       MessageList(messages = messageList, onDeleteClicked = {
+                         /** 2-2 messageList 상태 변경 발생 */
+                          messageList.remove(it)
+                       })
+                   }
                 }
             }
         }
@@ -30,14 +67,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AddMessage(onClicked:() -> Unit) {
+    Button(onClick = onClicked) {
+        Text(text = "메세지 추가")
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MessageRow(msg: Message, onDeleteClicked:(Message) -> Unit) {
+    Surface(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, color = Color.LightGray),
+        elevation = 10.dp
+    ) {
+        Column(
+            Modifier.padding(16.dp)
+        ) {
+            Text(text = "id : ${msg.id} / msg: ${msg.content}")
+            Button(onClick = { onDeleteClicked(msg) }) {
+                Text(text = "삭제")
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageList(messages: List<Message> , onDeleteClicked:(Message) -> Unit) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageRow(msg = message , onDeleteClicked = onDeleteClicked)
+        }
+    }
+}
+
+@Preview(showBackground = false)
 @Composable
 fun DefaultPreview() {
     MemoJetpackComposeTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/com/hyosik/android/memojetpackcompose/Message.kt
@@ -0,0 +1,6 @@
+package com.hyosik.android.memojetpackcompose
+
+data class Message(
+    val id: Int,
+    var content: String
+)
